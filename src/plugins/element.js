e450b7ebe49148dcf498b32fe45fde01b70ca770
panghaibin <panghaibin@shu.edu.cn>
2022-04-19 20:55:21 +0800

    upgrade element plus

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 export default (app) => {
