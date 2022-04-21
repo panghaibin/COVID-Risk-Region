c8a260ee213f4aa8063514d69341c3978690fd66
panghaibin <i@hbte.ch>
2022-04-21 20:26:56 +0800

    fix path error

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/COVID-Risk-Region/'
+      : '/',
 })
