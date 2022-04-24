ffb927d33a29aadf1ec05952c6e816585dedb199
panghaibin <i@hbte.ch>
2022-04-24 17:22:29 +0800

    fix font in firefox

--- src/components/RegionShow.vue
+++ src/components/RegionShow.vue
@@ -331,12 +331,19 @@ function list2tree(list, data) {
 }
 </style>
 <style>
+* {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+  'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
+
 .el-tree-node__content {
   height: auto;
 }
+
 .el-tree-node {
   white-space: break-spaces;
 }
+
 .tree-node {
   padding: 1px;
 }
