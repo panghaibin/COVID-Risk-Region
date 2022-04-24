7f0cecb77df482ce752901da0d3ed92dcc9269a8
panghaibin <i@hbte.ch>
2022-04-24 15:30:22 +0800

    refactor: auto import ui library

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,25 @@
-const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const {ElementPlusResolver} = require('unplugin-vue-components/resolvers')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/COVID-Risk-Region/'
-      : '/',
+    ? '/COVID-Risk-Region/'
+    : '/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components',
+      }
+    },
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
 })
