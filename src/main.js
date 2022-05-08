7d6e7615a5dc62d508f3cff3283beb64b544994f
panghaibin <i@hbte.ch>
2022-05-08 20:59:32 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import { createHead } from '@vueuse/head'
 import './registerServiceWorker'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import router from './router'
 
 const app = createApp(App)
+app.use(router)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
---
+++ src/views/HomeView.vue
@@ -0,0 +1,125 @@
+<template>
+  <Head>
+    <title>{{ title }}</title>
+  </Head>
+  <el-scrollbar height="100vh">
+    <div class="content-body">
+      <div class="content">
+        <el-container>
+          <el-header class="header">
+            <h1>{{ title }}</h1>
+          </el-header>
+          <el-main>
+            <RegionShow :data_url="api_url"></RegionShow>
+          </el-main>
+          <el-footer class="footer">
+            <FooterShow></FooterShow>
+          </el-footer>
+        </el-container>
+      </div>
+    </div>
+  </el-scrollbar>
+</template>
+
+<script>
+import RegionShow from "@/components/RegionShow";
+import FooterShow from "@/components/FooterShow";
+import { Head } from '@vueuse/head'
+import update from '@/mixins/update'
+
+export default {
+  name: "HomeView",
+  components: {
+    RegionShow,
+    FooterShow,
+    Head
+  },
+  computed: {
+    api_url() {
+      let url = process.env.NODE_ENV === 'development'
+          ? 'http://localhost/ccr-api/'
+          : 'https://raw.githubusercontent.com/panghaibin/RiskLevelAPI/api/';
+      let file_name = this.$route.params.fileName;
+      if (file_name) {
+        return url + file_name + '.json';
+      } else {
+        return url + 'latest.json';
+      }
+    }
+  },
+  data() {
+    return {
+      title: "全国疫情中高风险地区查询",
+    };
+  },
+  mixins: [update],
+
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+
+.content-body {
+  margin: 8px;
+}
+
+.content {
+  max-width: 900px;
+  min-width: 350px;
+  margin: 0 auto;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.header {
+  background-color: var(--el-color-primary-light-9);
+  color: var(--el-color-primary-dark-2);
+  height: auto !important;
+  text-align: center;
+  border-radius: 5px;
+}
+
+.footer {
+  bottom: 0;
+  height: auto;
+  text-align: center;
+}
+
+@media screen and (max-width: 768px) {
+  .el-scrollbar__bar {
+    display: none;
+  }
+}
+
+@media (prefers-color-scheme: dark) {
+  html, body {
+    background-color: #121212;
+  }
+
+  .header {
+    background-color: #191919;
+    color: #e2e2e2;
+  }
+
+  .el-input__wrapper {
+    background-color: #191919;
+    border-color: #73767a;
+    box-shadow: 0 0 0 1px #73767a;
+  }
+
+  p, h3, .el-input__inner {
+    color: #e2e2e2;
+  }
+
+  .el-tree {
+    --el-tree-node-hover-bg-color: #191919;
+    background-color: #121212;
+    color: #e2e2e2;
+  }
+}
+</style>
